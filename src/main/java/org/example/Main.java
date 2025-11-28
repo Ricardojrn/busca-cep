63e947d80b45a61110e831d7fdce89c903d5ba2a
Ricardojrn <cadonascimentosilva@hotmail.com>
2025-11-28 17:44:20 -0300

    Criando gerador de arquivos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,27 @@
 package org.example;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
-        Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
-        System.out.println(novoEndereco);
+
+        System.out.println("Digite o CEP para consultar: ");
+        var cep = input.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("FINALIZANDO");
+        }
     }
 }
